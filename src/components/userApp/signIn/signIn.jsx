c956126d5db53c7017619edb8fc1ac4330d83d3e
Oleksii Yashchuk <YashchukAlex5@gmail.com>
2019-04-11 11:38:53 +0300

    add routes, signIn form

---
+++ src/components/userApp/signIn/signIn.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import LoginContainer from '../../../containers/signIn/signIn';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <LoginContainer/>    
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/constants/auth.js
@@ -0,0 +1,15 @@
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
+
+export const TOKEN_NAME = 'TOKEN_NAME';
+
+export const BASE_API = 'https://myforum.azurewebsites.net';
